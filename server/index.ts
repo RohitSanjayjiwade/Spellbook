9f3750e55013a764cb105218e6e808dc38fd8c39
Rohit Jiwade <rohitjiwade2019@gmail.com>
2024-04-25 10:35:12 +0530

    build Prisma And tRPC

---
+++ server/index.ts
@@ -0,0 +1,9 @@
+import { spellsRouter } from "./routers/spells";
+import { router } from "./trpc";
+
+export const appRouter = router({
+	spells: spellsRouter,
+});
+
+
+export type AppRouter = typeof appRouter;
